a0e4df4a8a231abdca5e931218e84323587b5fc6
ReeseGanaway <93220118+ReeseGanaway@users.noreply.github.com>
2024-05-17 22:38:23 -0700

    signup page can now add a user to Appwrite backend

--- src/_auth/forms/SignupForm.tsx
+++ src/_auth/forms/SignupForm.tsx
@@ -9,6 +9,7 @@ import {useForm} from "react-hook-form"
 import { SingupValidation } from "@/lib/validation"
 import Loader from "@/components/shared/Loader"
 import { Link } from "react-router-dom"
+import { createUserAccount } from "@/lib/appwrite/api"
 
 const SignupForm = () => {
   const isLoading = false;
@@ -24,7 +25,9 @@ const SignupForm = () => {
  
 
   async function onSubmit(values: z.infer<typeof SingupValidation>) {
-    // const newUser = await createUserAccount(values);
+    const newUser = await createUserAccount(values);
+
+    console.log(newUser);
     
   }
 
--- src/lib/validation/index.ts
+++ src/lib/validation/index.ts
@@ -2,6 +2,6 @@ import { z } from "zod"
 
 export const SingupValidation = z.object({
     email: z.string().email(),
-    username: z.string().min(1, {message: "Username cannot be blank"}).max(20),
-    password: z.string().min(8, {message: "Password must be at least 8 characters"})
+    username: z.string().min(1, "Username cannot be blank").max(20, 'Username cannot exceed 20 characters'),
+    password: z.string().min(8, "Password must be at least 8 characters")
   })
